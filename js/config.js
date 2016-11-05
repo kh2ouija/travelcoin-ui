8b813e8b1100f2b07e011b1298934d0bb5e01087
costin <costinaldea@yahoo.co.uk>
2016-11-05 17:41:21 +0200

    fixed states

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
     IdleProvider.idle(5); // in seconds
     IdleProvider.timeout(120); // in seconds
 
-    $urlRouterProvider.otherwise("/commerce/products_grid");
+    $urlRouterProvider.otherwise("/assets/assets-grid");
 
 
 
@@ -23,9 +23,9 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
 
     $stateProvider
 
-        .state('commerce', {
+        .state('assets', {
             abstract: true,
-            url: "/commerce",
+            url: "/assets",
             templateUrl: "views/common/content.html",
             resolve: {
                 loadPlugin: function ($ocLazyLoad) {
@@ -39,197 +39,18 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
                         }
                     ]);
                 }
-            },
-            controller: 'AppController'
-        })
-        .state('commerce.products_grid', {
-            url: "/products_grid",
-            templateUrl: "views/ecommerce_products_grid.html",
-            data: { pageTitle: 'Assets list' }
-        })
-        .state('commerce.product_list', {
-            url: "/product_list",
-            templateUrl: "views/ecommerce_product_list.html",
-            data: { pageTitle: 'E-commerce product list' },
-            controller: 'AssetController'
-        })
-        .state('commerce.orders', {
-            url: "/orders",
-            templateUrl: "views/ecommerce_orders.html",
-            data: { pageTitle: 'E-commerce orders' }
-        })
-        .state('commerce.product', {
-            url: "/product",
-            templateUrl: "views/ecommerce_product.html",
-            data: { pageTitle: 'Product edit' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            files: ['css/plugins/summernote/summernote.css','css/plugins/summernote/summernote-bs3.css','js/plugins/summernote/summernote.min.js']
-                        },
-                        {
-                            name: 'summernote',
-                            files: ['css/plugins/summernote/summernote.css','css/plugins/summernote/summernote-bs3.css','js/plugins/summernote/summernote.min.js','js/plugins/summernote/angular-summernote.min.js']
-                        }
-                    ]);
-                }
             }
-
+        })
+        .state('assets.assets_grid', {
+            url: "/assets-grid",
+            templateUrl: "views/assets_grid.html",
+            data: { pageTitle: 'Assets list' },
         })
 
-        .state('dashboards', {
-            abstract: true,
-            url: "/dashboards",
-            templateUrl: "views/common/content.html",
-        })
-        .state('dashboards.dashboard_1', {
-            url: "/dashboard_1",
-            templateUrl: "views/dashboard_1.html",
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-
-                            serie: true,
-                            name: 'angular-flot',
-                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
-                        },
-                        {
-                            name: 'angles',
-                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
-                        },
-                        {
-                            name: 'angular-peity',
-                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('dashboards.dashboard_2', {
-            url: "/dashboard_2",
-            templateUrl: "views/dashboard_2.html",
-            data: { pageTitle: 'Dashboard 2' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            serie: true,
-                            name: 'angular-flot',
-                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
-                        },
-                        {
-                            files: ['js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js', 'js/plugins/jvectormap/jquery-jvectormap-2.0.2.css']
-                        },
-                        {
-                            files: ['js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js']
-                        },
-                        {
-                            name: 'ui.checkbox',
-                            files: ['js/bootstrap/angular-bootstrap-checkbox.js']
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('dashboards.dashboard_3', {
-            url: "/dashboard_3",
-            templateUrl: "views/dashboard_3.html",
-            data: { pageTitle: 'Dashboard 3' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            name: 'angles',
-                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
-                        },
-                        {
-                            name: 'angular-peity',
-                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
-                        },
-                        {
-                            name: 'ui.checkbox',
-                            files: ['js/bootstrap/angular-bootstrap-checkbox.js']
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('dashboards_top', {
-            abstract: true,
-            url: "/dashboards_top",
-            templateUrl: "views/common/content_top_navigation.html",
-        })
-        .state('dashboards_top.dashboard_4', {
-            url: "/dashboard_4",
-            templateUrl: "views/dashboard_4.html",
-            data: { pageTitle: 'Dashboard 4' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            name: 'angles',
-                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
-                        },
-                        {
-                            name: 'angular-peity',
-                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
-                        },
-                        {
-                            serie: true,
-                            name: 'angular-flot',
-                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('dashboards.dashboard_4_1', {
-            url: "/dashboard_4_1",
-            templateUrl: "views/dashboard_4_1.html",
-            data: { pageTitle: 'Dashboard 4' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            name: 'angles',
-                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
-                        },
-                        {
-                            name: 'angular-peity',
-                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
-                        },
-                        {
-                            serie: true,
-                            name: 'angular-flot',
-                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('app', {
-            abstract: true,
-            url: "/app",
-            templateUrl: "views/common/content.html",
-        })
-        .state('app.contacts', {
-            url: "/contacts",
-            templateUrl: "views/contacts.html",
-            data: { pageTitle: 'Contacts' }
-        })
-        .state('app.profile', {
-            url: "/profile",
-            templateUrl: "views/profile.html",
-            data: { pageTitle: 'Profile' }
-        })
-
-        .state('login_two_columns', {
-            url: "/login_two_columns",
-            templateUrl: "views/login_two_columns.html",
-            data: { pageTitle: 'Please authenticate', specialClass: 'gray-bg' },
-            controller: 'AppController'
+        .state('login', {
+            url: "/login",
+            templateUrl: "views/responsive-login.html",
+            data: { pageTitle: 'Please authenticate', specialClass: 'gray-bg' }
         })
 
         .state('landing', {
