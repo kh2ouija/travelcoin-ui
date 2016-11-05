d8cfb3634777c5fa2fbe3cf511ad06a818ca8360
Stefan <kh2ouija@yahoo.com>
2016-11-05 20:30:21 +0200

    claim controller

--- js/controllers.js
+++ js/controllers.js
@@ -94,6 +94,21 @@ angular
         $scope.marketData()
 
      })
+     .controller('ClaimController', function ClaimController($scope, $state, $http) {
+    $scope.providers = [];
+
+    this.init = function() {
+        console.log('ClaimController.init()');
+        $http({
+            method: 'GET',
+            url: 'http://travelcoin-api.herokuapp.com/providers'
+        }).then(function(response) {
+            $scope.providers = response.data;
+        });
+    }
+
+    this.init();
+    })
      .service('userService', UserService)
     .factory('sessionFactory', function ($http, $log) {
 
